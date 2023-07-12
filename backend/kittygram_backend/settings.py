8ef3447103ed930fbd38e4bf9e51a6c670e06035
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-07-12 14:17:19 +0300

    Fixed after 1sr review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('TOKEN', default='SECRET_TOKEN')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yakittygram.zapto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
